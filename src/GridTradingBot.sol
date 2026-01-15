cab775f8da38c84158c250f15ed06c11fb47a82c
MariliaBontempo <mariliabontempo@gmail.com>
2026-01-15 10:31:44 -0300

    Add Foundry dependencies and fix OpenZeppelin import paths
    - Add forge-std v1.14.0 as git submodule
    - Add openzeppelin-contracts v4.9.6 as git submodule (for safeApprove compatibility)
    - Create remappings.txt for Foundry import resolution
    - Fix ReentrancyGuard import path from utils/ (v5.x) to security/ (v4.x)
    - Project now compiles successfully with forge build

--- src/GridTradingBot.sol
+++ src/GridTradingBot.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import { IGridTradingBot } from "./interfaces/IGridTradingBot.sol";
 import { IAutomationCompatibleInterface } from "./interfaces/IAutomationCompatible.sol";
 import { TWAPLib } from "./libraries/TWAPLib.sol";
